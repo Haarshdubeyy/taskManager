626e3e45bc0794b545388ecd8e0a55b016f4dd54
Harsh Dubey <123404368+Haarshdubeyy@users.noreply.github.com>
2025-04-16 12:17:18 +0530

    Update utils.js

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -4,4 +4,4 @@ export const notify = (message, type) => {
     toast[type](message);
 }
 
-export const API_URL = 'https://task-manager-alpha-pearl.vercel.app';
+export const API_URL = 'https://taskmanager-backend-production-6967.up.railway.app';
